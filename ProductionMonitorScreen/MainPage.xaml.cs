d970e2266e3be3bf4330c9ce3b677204158cd321
Metisria Geo <metisria@hotmail.com>
2014-07-11 11:19:27 +0800

    write examples

---
+++ ProductionMonitorScreen/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+using Unis.XamlConfiguration;
+using Unis.XamlConfiguration.Analytics;
+using Unis.XamlConfiguration.Data;
+using Unis.XamlConfiguration.Drawing;
+
+namespace Unis.ProductionMonitorScreen
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+ 
+    }
+}
---
+++ ProductionMonitorScreen/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Production Monitor Screen Samples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Metisria Geo")]
+[assembly: AssemblyProduct("Unis.XamlConfiguration.Samples")]
+[assembly: AssemblyCopyright("Copyright © metisria@outlook.com 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2a04f147-f238-4a50-b063-2725fc3caadb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("6.14.3.16")]
+[assembly: AssemblyFileVersion("6.14.3.16")]
+[assembly: AssemblyInformationalVersion("6.14.3.16")]
