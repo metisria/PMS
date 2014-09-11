1384404c1359c5ff6e6560721ead11b7304d0515
Metisria Geo <metisria@hotmail.com>
2014-09-11 13:34:24 +0800

    ignore xap files

---
+++ ProductionMonitorScreen/CustomGraphics/泵类.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Unis.ProductionMonitorScreen
+{
+	public partial class 泵类 : UserControl
+	{
+		public 泵类()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
