386802dcfb9d1eafce10f890d0de6691f073fde2
Metisria Geo <metisria@hotmail.com>
2014-09-12 08:58:40 +0800

    changed

---
+++ ProductionMonitorScreen/CustomGraphics/阀门类.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Unis.ProductionMonitorScreen
+{
+	public partial class 阀门类 : UserControl
+	{
+		public 阀门类()
+		{
+			// 为初始化变量所必需
+			InitializeComponent();
+		}
+	}
+}
